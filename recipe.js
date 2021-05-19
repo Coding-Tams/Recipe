963467b10f00565d9486726f8db614992e7ce7df
Coding-Tams <82816924+Coding-Tams@users.noreply.github.com>
2021-05-19 11:28:57 +0100

    Create recipe.js
    add javascript to the html of the recipe respository

---
+++ recipe.js
@@ -0,0 +1,37 @@
+/*
+
+Recipe Title
+
+servings = number
+ingredients = array of strings
+directions =  array of strings
+
+Title = Spag Bol
+Severings = 4
+ingredients = spaghetti, mince, tomato sauce, cheese
+
+let favourite recipe {
+    recipeTitle: 'spag bol',
+    servings: '4',
+    ingredients: [ ] 
+
+}
+
+directions 
+    boil water in pan 1
+    open spaghetti
+    put spaghetti in pan 1
+    put mince in pan 2 with tomoato sauce
+    empty pan 2 into pan 1
+    mix together 
+    serve
+
+to loop over ingredients
+For loop
+    counter 0
+    condition = counter < ingredients.length
+    counter + 1
+
+    */
+
+   
